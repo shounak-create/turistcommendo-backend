4a5fb57723f4e9158a58aebd64e7331a4eb38761
Shounak Pandit <shounakpandit47@gmail.com>
2024-06-08 19:13:52 +0530

    first commit of the project.

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+require('dotenv').config();
+
+const app = express()
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(port,() =>{
+    console.log(`Server is running on port ${port}`)
+});
+
+app.get('',() =>{
+    console.log("Server is running");
+})
